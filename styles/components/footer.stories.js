47eb9c6e1a5109854410af169d28bbf305175475
Carolin Weitkamp <c.weitkamp@posteo.de>
2022-02-10 09:35:58 +0100

    new Card

--- styles/components/footer.stories.js
+++ styles/components/footer.stories.js
@@ -1,3 +1,25 @@
 export default {
   title: "Components/footer",
 };
+export default {
+  title: "Components/Footer",
+};
+
+export const Error = () => /* html */ `
+<footer>
+<ul>
+  <li>
+    <a href="#home"><strong>Home</strong></a>
+  </li>
+  <li>
+    <a href="#boockmarks"><strong>Boockmarks</strong></a>
+  </li>
+  <li>
+    <a href="#create"><strong>Create</strong></a>
+  </li>
+  <li>
+    <a href="#profile"><strong>Profile</strong></a>
+  </li>
+</ul>
+</footer>
+  `;
---
+++ styles/components/section.stories.js
@@ -0,0 +1,14 @@
+export default {
+  title: "Components/Karte",
+};
+
+export const Error = () => /* html */ `
+<section>
+<h3>Wofür steht XYZ?</h3>
+<div class="icon">MARK</div>
+<p>
+  In der Umgangspache viel genutze Redeweise Ganz klasse. so und jetzt
+  du...
+</p>
+</section>
+  `;
