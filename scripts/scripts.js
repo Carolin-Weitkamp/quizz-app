9a85e878dfb38d0966aa213e15b20a35664ac4c3
Carolin Weitkamp <c.weitkamp@posteo.de>
2022-02-17 13:04:55 +0100

    changes counter

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,17 @@
 const showAnswerButton = document.querySelector('[data-js="answer-button"]');
 const firstAnswer = document.querySelector('[data-js="first-answer"]');
 
-showAnswerButton.addEventListener('click', () => {
+showAnswerButton?.addEventListener('click', () => {
   firstAnswer.classList.toggle('display-toggle');
+});
+
+const inputForm = document.querySelector('[data-js="input-form"]');
+const inputCounter = document.querySelector('[data-js="input-counter"]');
+
+inputForm?.addEventListener('input', () => {
+  if (inputForm.value.length < 40) {
+    inputCounter.textContent = inputForm.value.length;
+  } else {
+    inputCounter.textContent = 'Your limit is reached';
+  }
 });
